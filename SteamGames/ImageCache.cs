98d61db5b7a48ae243a6935113bc6f4587711979
Matthijs van der Vleuten <zr40.nl@gmail.com>
2013-06-29 23:25:33 +0200

    indent

--- SteamGames/ImageCache.cs
+++ SteamGames/ImageCache.cs
@@ -21,41 +21,41 @@ namespace SteamGames
 				return;
 
 			Task.Run(() =>
-			         {
-				         string file = Path.Combine(cachePath, string.Format("{0}_{1}", app, part));
-				         if (!File.Exists(file))
-				         {
-					         string url = string.Format("http://media.steampowered.com/steamcommunity/public/images/apps/{0}/{1}.jpg", app, part);
+			{
+				string file = Path.Combine(cachePath, string.Format("{0}_{1}", app, part));
+				if (!File.Exists(file))
+				{
+					string url = string.Format("http://media.steampowered.com/steamcommunity/public/images/apps/{0}/{1}.jpg", app, part);
 
-					         try
-					         {
-						         WebRequest r = WebRequest.Create(url);
-						         using (WebResponse response = r.GetResponse())
-						         using (Stream s = response.GetResponseStream())
-						         {
-							         using (FileStream f = File.OpenWrite(file))
-							         {
-								         s.CopyTo(f);
-							         }
-						         }
-					         }
-					         catch (WebException)
-					         {
-						         return;
-					         }
-				         }
+					try
+					{
+						WebRequest r = WebRequest.Create(url);
+						using (WebResponse response = r.GetResponse())
+						using (Stream s = response.GetResponseStream())
+						{
+							using (FileStream f = File.OpenWrite(file))
+							{
+								s.CopyTo(f);
+							}
+						}
+					}
+					catch (WebException)
+					{
+						return;
+					}
+				}
 
-				         using (FileStream stream = File.OpenRead(file))
-				         {
-					         receiver.ReceiveImage(stream);
-				         }
+				using (FileStream stream = File.OpenRead(file))
+				{
+					receiver.ReceiveImage(stream);
+				}
 
-				         if (RedrawNotification != null)
-				         {
-					         Console.WriteLine("Notification");
-					         RedrawNotification();
-				         }
-			         });
+				if (RedrawNotification != null)
+				{
+					Console.WriteLine("Notification");
+					RedrawNotification();
+				}
+			});
 		}
 	}
 }
